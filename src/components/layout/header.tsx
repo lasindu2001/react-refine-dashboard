d55b07dc4ac7e7e4167e58da3421bde4b952446d
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-30 09:36:34 +0530

    complete header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import CurrentUser from './current-user'
+import React from "react";
+import { Layout, Space, theme } from "antd";
+import CurrentUser from "./current-user";
+
+const { useToken } = theme;
 
 const Header = () => {
-  return (
-    <CurrentUser />
-  )
-}
+  const { token } = useToken();
 
-export default Header
+  const headerStyles: React.CSSProperties = {
+    backgroundColor: '#7596a1',
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: "0px 24px",
+    height: "64px",
+    position: "sticky",
+    top: 0,
+    zIndex: 999,
+  };
+
+  return (
+    <Layout.Header style={headerStyles}>
+      <Space align="center" size="middle">
+        <CurrentUser />
+      </Space>
+    </Layout.Header>
+  );
+};
+
+export default Header;
