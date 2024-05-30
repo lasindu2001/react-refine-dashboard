49662fae91710a3bd038d5b214f14173edc6620d
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-30 13:02:20 +0530

    complete upcoming events

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,10 +1,30 @@
 import { CalendarOutlined } from '@ant-design/icons'
-import { Card, List } from 'antd'
+import { Badge, Card, List } from 'antd'
 import React, { useState } from 'react'
 import { Text } from '../text'
+import UpcomingEventsSkeleton from '../skeleton/upcoming-events'
+import { getDate } from '@/utilities/helpers'
+import { useList } from '@refinedev/core'
+import { DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY } from '@/graphql/queries'
+import dayjs from 'dayjs'
 
 const UpcomingEvents = () => {
-    const [isLoading, setisLoading] = useState(true);
+    const { data, isLoading } = useList({
+        resource: 'events',
+        pagination: { pageSize: 5 },
+        sorters: [{
+            field: 'startDate',
+            order: 'asc'
+        }],
+        filters: [{
+            field: 'startDate',
+            operator: 'gte',
+            value: dayjs().format('YYYY-MM-DD')
+        }],
+        meta: {
+            gqlQuery: DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY
+        }
+    })
 
     return (
         <Card
@@ -20,7 +40,7 @@ const UpcomingEvents = () => {
                     <CalendarOutlined />
                     <Text
                         size='sm'
-                        style={{ marginLeft: '0.7rem'}}
+                        style={{ marginLeft: '0.7rem' }}
                     >
                         Upcoming Events
                     </Text>
@@ -30,11 +50,41 @@ const UpcomingEvents = () => {
             {isLoading ? (
                 <List
                     itemLayout='horizontal'
-                    dataSource={Array.from({ length: 5}).map((_, index) => ({
+                    dataSource={Array.from({ length: 5 }).map((_, index) => ({
                         id: index
-                    }))}></List>
+                    }))}
+                    renderItem={() => <UpcomingEventsSkeleton />}
+                />
             ) : (
-                <List></List>
+                <List
+                    itemLayout='horizontal'
+                    dataSource={data?.data || []}
+                    renderItem={(item) => {
+                        const renderDate = getDate(item.startDate, item.endDate)
+
+                        return (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={<Badge color={item.color} />}
+                                    title={<Text size='xs'>{renderDate}</Text>}
+                                    description={<Text ellipsis={{ tooltip: true }} strong>{item.title}</Text>}
+                                />
+                            </List.Item>
+                        )
+                    }}
+                />
+            )}
+            {!isLoading && data?.data.length === 0 && (
+                <span
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: '200px',
+                    }}
+                >
+                    No upcoming events
+                </span>
             )}
         </Card>
     )
