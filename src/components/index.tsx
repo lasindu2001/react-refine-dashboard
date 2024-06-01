8771b2315a344b3c24e76513f7d19b34f42404e5
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-01 10:14:05 +0530

    complete latest activities

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -6,6 +6,7 @@ import KanbanColumnSkeleton from "./skeleton/kanban";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
 import ProjectCardSkeleton from "./skeleton/project-card";
 import DashboardTotalCountCard from "./home/total-count-card";
+import LatestActivities from "./home/latest-activities";
 
 export { 
     UpcomingEvents, 
@@ -16,4 +17,5 @@ export {
     LatestActivitiesSkeleton,
     ProjectCardSkeleton,
     DashboardTotalCountCard,
+    LatestActivities
 };
